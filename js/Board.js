0b78f67458836972f96372d64968e31f5522c83c
basiacz <basiaczerw@gmail.com>
2018-12-16 16:13:39 +0100

    12.7

---
+++ js/Board.js
@@ -0,0 +1,25 @@
+var board = {
+  name: 'Tablica Kanban',
+  addColumn: function (column) {
+    this.element.appendChild(column.element);
+    initSortable(column.id); //About this feature we will tell later
+  },
+  element: document.querySelector('#board .column-container')
+};
+
+document.querySelector('#board .create-column').addEventListener('click', function () {
+  var name = prompt('Enter a column name');
+
+  if (name !== '' && name !== null) {
+    var column = new Column(name);
+    board.addColumn(column);
+  }
+});
+
+function initSortable(id) {
+  var el = document.getElementById(id);
+  var sortable = Sortable.create(el, {
+    group: 'kanban',
+    sort: true
+  });
+}
